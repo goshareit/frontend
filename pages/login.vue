79346a554ea94d611d707caa51281b854889c9c2
simpleauthority <jacob@algorithmjunkie.com>
2019-07-19 17:33:25 -0700

    add login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-form @submit.prevent="handleLogIn()">
+  <b-form ref="form" @submit.prevent="handleLogIn()">
     <b-form-group
       id="username-input-group"
       label="Username:"
@@ -13,15 +13,24 @@
         :state="usernameOk"
         required
         placeholder="Enter Username"
-        @change="checkUsername()"
       />
+      <b-form-invalid-feedback :state="usernameOk">
+        <p v-if="!usernameIsValidForm">
+          Your username can only contain numbers, lowercase, and uppercase letters.
+        </p>
+        <p v-else-if="usernameTooShort">
+          Your username cannot be shorter than 4 characters.
+        </p>
+        <p v-else-if="usernameTooLong">
+          Your username cannot be longer than 24 characters.
+        </p>
+      </b-form-invalid-feedback>
     </b-form-group>
-
     <b-form-group
       id="password-input-group"
-      label="Choose a Password:"
+      label="Password:"
       label-for="password-input"
-      description="Use a strong password, and never one you've used elsewhere"
+      description="Enter your password to sign in"
     >
       <b-form-input
         id="password-input"
@@ -33,10 +42,60 @@
         placeholder="Enter Password"
       />
       <b-form-invalid-feedback :state="passwordOk">
-        <p v-if="passwordTooLong">
+        <p v-if="passwordTooShort">
+          Your password must be at least 8 characters long.
+        </p>
+        <p v-else-if="passwordTooLong">
           Your password cannot be longer than 71 characters.
         </p>
       </b-form-invalid-feedback>
     </b-form-group>
+
+    <b-button type="submit" variant="info">
+      Sign Up!
+    </b-button>
   </b-form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  computed: {
+    usernameIsValidForm() {
+      return /^[a-zA-Z0-9]+$/.test(this.username)
+    },
+    usernameTooShort() {
+      return this.username.length < 4
+    },
+    usernameTooLong() {
+      return this.username.length > 24
+    },
+    usernameOk() {
+      if (this.username.length === 0) return null
+      return this.usernameIsValidForm && !this.usernameTooShort && !this.usernameTooLong
+    },
+    passwordTooShort() {
+      return this.password.length < 8
+    },
+    passwordTooLong() {
+      return this.password.length > 71
+    },
+    passwordOk() {
+      if (this.password.length === 0) return null
+      return !this.passwordTooShort && !this.passwordTooLong
+    }
+  },
+  methods: {
+    handleLogIn() {
+      if (!this.usernameOk || !this.passwordOk) return
+      // eslint-disable-next-line no-console
+      console.log('we out here')
+    }
+  }
+}
+</script>
